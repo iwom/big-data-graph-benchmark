85eb2e22b0287c2b1461043ded1e67fb93c055aa
iwom <17498068+iwom@users.noreply.github.com>
2021-05-09 15:18:32 +0200

    scripts | update flink gelly

--- scripts/flink-gelly/run_flink_gelly.sh
+++ scripts/flink-gelly/run_flink_gelly.sh
@@ -3,6 +3,8 @@
 # Run on master node:
 # Usage ./run_flink_gelly.sh pagerank hdfs://flink-m:8020/user/input.txt hdfs://flink-m:8020/user/output 100
 # Usage ./run_flink_gelly.sh degrees hdfs://flink-m:8020/user/input.txt hdfs://flink-m:8020/user/output
+# Usage ./run_flink_gelly.sh triangles hdfs://flink-m:8020/user/input.txt hdfs://flink-m:8020/user/output
+# Usage ./run_flink_gelly.sh sssp hdfs://flink-m:8020/user/input.txt hdfs://flink-m:8020/user/output
 
 # 4) Run
 flink run -m yarn-cluster ~/big-data-graph-benchmark/flink/flink-gelly/target/flink-gelly-1.0-SNAPSHOT.jar "$1" "$2" "$3" "$4"
