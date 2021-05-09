504017ad8f67e7a5b9d69ff8a099135cbcedf248
iwom <17498068+iwom@users.noreply.github.com>
2021-05-09 12:57:57 +0200

    scripts | fix flink-core install mvn

--- scripts/flink/build_flink.sh
+++ scripts/flink/build_flink.sh
@@ -3,6 +3,7 @@
 # Run on master node:
 
 # 1) Build sources for flink-core-test
+sudo apt-get install maven -y
 git clone https://github.com/iwom/big-data-graph-benchmark.git
 cd ~/big-data-graph-benchmark/flink/flink-core || exit
 mvn clean package
