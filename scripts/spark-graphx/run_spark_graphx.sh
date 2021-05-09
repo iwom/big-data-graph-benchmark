b9b5dddf4a492eea4d25e398fe40852fa42c207e
iwom <17498068+iwom@users.noreply.github.com>
2021-05-09 19:07:26 +0200

    script | fix spark graphx run cmd

--- scripts/spark-graphx/run_spark_graphx.sh
+++ scripts/spark-graphx/run_spark_graphx.sh
@@ -5,7 +5,7 @@
 # Usage ./run_spark_graphx.sh degrees hdfs://spark-m:8020/user/input.txt hdfs://spark-m:8020/user/output
 
 # 1) Run
-spark-submit --class SparkGraphxTest --master yarn ~/big-data-graph-benchmark/spark/spark-core/target/scala-2.12/spark-core.jar "$1" "$2" "$3" "$4"
+spark-submit --class SparkGraphxTest --master yarn ~/big-data-graph-benchmark/spark/spark-graphx/target/scala-2.12/spark-graphx.jar "$1" "$2" "$3" "$4"
 
 # 2) Show result
 hdfs dfs -cat "$3"/* | head -n 10
